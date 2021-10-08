e4797bba20dd8d1bba48c6fb105faffa5ce31aa5
Victoria045 <victoriaberyl45@gmail.com>
2021-10-08 08:14:24 +0300

    initialize object

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -4,73 +4,71 @@ import { Quote } from '../quote';
 @Component({
   selector: 'app-quote',
   templateUrl: './quote.component.html',
-  styleUrls: ['./quote.component.css']
+  styleUrls: ['./quote.component.css'],
 })
-
 export class QuoteComponent implements OnInit {
-  quotes:Quote[] = [
-    new Quote("Victoria Beryl", "Injustice anywhere is a threat to justice everywhere.", "Martin Luther King", new Date(2021,9,11)),
-    new Quote("Chris Mark", "In the end we all become stories.", "Paul Mark", new Date(2021,9,6)),
+  quotes: Quote[] = [
+    new Quote(
+      'Victoria Beryl',
+      "Don't bend; don't water it down; don't try to make it logical; don't edit your own soul according to the fashion. Rather, follow your most intense obsessions mercilessly.",
+      'Franz Kafka',
+      new Date(2021, 9, 11)
+    ),
+    new Quote(
+      'Chris Mark',
+      'A word after a word after a word is power.',
+      'Margaret Atwood',
+      new Date(2021, 9, 6)
+    ),
   ];
 
-
-  postNewQuote(quote:Quote){
+  postNewQuote(quote: Quote) {
     let quoteLength = this.quotes.length;
-    quote.postedDate = new Date(quote.postedDate)
-    this.quotes.push(quote)
+    quote.postedDate = new Date(quote.postedDate);
+    this.quotes.push(quote);
   }
 
-  toggleDetails(index:number){
+  toggleDetails(index: number) {
     this.quotes[index].showDetails = !this.quotes[index].showDetails;
   }
 
-  deleteQuote(isComplete:boolean, index:number){
-    // if (isComplete) {
-    //   this.quotes.splice(index,1);
-    // }
-
+  deleteQuote(isComplete: boolean, index: number) {
     if (isComplete) {
-      let toDelete = confirm(`Are you sure you want to delete ${this.quotes[index].quote}?`)
+      let toDelete = confirm(
+        `Are you sure you want to delete ${this.quotes[index].quote}?`
+      );
 
       if (toDelete) {
-        this.quotes.splice(index,1);
+        this.quotes.splice(index, 1);
       }
-      
     }
   }
 
-  // addQuote(chosenQuote) {
-  //   this.quotes.push(chosenQuote)
-  // }
+  upvote(i: number) {
+    this.quotes[i].upvotes++;
+  }
+  downvote(i: number) {
+    this.quotes[i].downvotes++;
+  }
 
-  upvote(i:number) {
-     this.quotes[i].upvotes ++;
-   }
-   downvote(i:number) {
-     this.quotes[i].downvotes  ++;
-   }
-  // delQuote(i) {
-  //   this.quotes.splice(i, 1)
-  // }
+  preNum?: number;
+  lastNum?: number;
+  counter?: number;
 
-  preNum?:number;
-  lastNum?:number;
-  counter?:number;
+  highestUpvote() {
+    this.preNum = 0;
+    this.lastNum = 0;
 
-  highestUpvote(){
-    this.preNum = 0
-    this.lastNum = 0
-
-    for(this.counter=0 ; this.counter < this.quotes.length; this.counter++) {
+    for (this.counter = 0; this.counter < this.quotes.length; this.counter++) {
       this.lastNum = this.quotes[this.counter].upvotes;
-      if(this.lastNum > this.preNum){this.preNum = this.lastNum}
+      if (this.lastNum > this.preNum) {
+        this.preNum = this.lastNum;
+      }
     }
-    return  this.preNum
+    return this.preNum;
   }
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
