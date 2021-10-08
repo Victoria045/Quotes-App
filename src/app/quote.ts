f5b85cdc3ec76d75bb57d2707fda7776f0edc54a
Victoria045 <victoriaberyl45@gmail.com>
2021-10-08 08:14:47 +0300

    modify constructor

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -2,7 +2,7 @@ export class Quote {
   public upvotes:number
   public downvotes:number
   showDetails: boolean;
-  // constructor(public name:string, public author:string,public description:string,public postedDate: Date)
+  
   constructor(public publisher:string, public quote:string, public author:string,public postedDate: Date){
     this.upvotes =0;
     this.downvotes =0;
