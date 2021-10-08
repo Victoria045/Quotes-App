1545d50f6edee959b679adac184e0f064bb092a4
Victoria045 <victoriaberyl45@gmail.com>
2021-10-08 06:33:14 +0300

    configure files in the root file

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
+import { QuoteComponent } from './quote/quote.component';
+import { HighlightDirective } from './highlight.directive';
+import { DateCountPipe } from './date-count.pipe';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    QuoteDetailComponent,
+    QuoteComponent,
+    HighlightDirective,
+    DateCountPipe,
+    QuoteFormComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
